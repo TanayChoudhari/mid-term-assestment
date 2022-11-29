a064ea2984cf90fad5a24e6a38b2d390ee0cda25
TanayChoudhari <tanaydemoacc123@gmail.com>
2022-11-29 18:34:31 +0530

    Intial Commit

---
+++ Plugins/DisableCustomerRegistration.php
@@ -0,0 +1,33 @@
+<?php
+namespace Cognixia\CustomerDisableRegistration\Plugins;
+
+use Magento\Customer\Model\Registration;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class DisableCustomerRegistration
+{
+    protected $scopeConfig;
+    const XML_DISABLE_CUSTOMER_REGISTRATION = 'customer/create_account/disable_customer_registration';
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    )
+    { 
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function afterIsAllowed( Registration $subject, $result)
+    {
+        if($this->scopeConfig->isSetFlag(
+            self::XML_DISABLE_CUSTOMER_REGISTRATION, 
+            $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT, 
+            $scopeCode = null)
+        ){
+            return false;
+        }else{
+            return true;
+        }
+
+        return $result;
+    }
+}
